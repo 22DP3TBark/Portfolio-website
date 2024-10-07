9d17d1545f85d07bcac0a77e5814270fa82dacef
22DP3TBark <131261277+22DP3TBark@users.noreply.github.com>
2024-10-07 11:48:24 +0000

    made the dark mode button work

--- Script.js
+++ Script.js
@@ -1,7 +1,27 @@
+console.log("Script loaded");
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
 
+}
+
+let darkModeEnabled = false;
+const darkModeButton = document.getElementById("darkModeButton");
+darkModeButton.addEventListener("click", () => {
+    darkModeEnabled = !darkModeEnabled;
+    if  (darkModeEnabled) {
+        EnableDarkMode();
+    }   else {
+        DisableDarkMode();
+    }
+});
+
+const EnableDarkMode = () => {
+    document.body.classList.add("dark-mode");
+}
+
+const DisableDarkMode = () => {
+    document.body.classList.remove("dark-mode");
 }
